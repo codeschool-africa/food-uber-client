db89b57403e397d230d580c79831f768c1bd528d
Benedict <bmsteve96@gmail.com>
2020-12-25 02:01:12 +0300

    added UI for cart cards

--- food-uber/src/components/cart/Cart.js
+++ food-uber/src/components/cart/Cart.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react"
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai"
+
+import "./cart.sass"
 
 const CartCard = ({ id, name, description, food_image, cost }) => {
   let [number, setNumber] = useState(1)
@@ -27,9 +30,13 @@ const CartCard = ({ id, name, description, food_image, cost }) => {
         <div className="name">{name}</div>
         <div className="cost">&#64; Tshs {cost}</div>
         <div className="counter">
-          <span onClick={minus}>-</span>
-          <span>{number}</span>
-          <span onClick={add}>+</span>
+          <span onClick={minus} className="span">
+            <AiOutlineMinus className="icon" />
+          </span>
+          <span className="number">{number}</span>
+          <span onClick={add} className="span">
+            <AiOutlinePlus className="icon" />
+          </span>
         </div>
       </div>
     </div>
