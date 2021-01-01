852257206fe94001cc8baadbcbab8b032bb772aa
Benedict <bmsteve96@gmail.com>
2021-01-01 03:22:24 +0300

    landing page UI

--- food-uber/src/components/testimonies/Testimonies.js
+++ food-uber/src/components/testimonies/Testimonies.js
@@ -1,10 +1,57 @@
 import React from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
+import SwiperCore, { Pagination, Navigation } from "swiper"
 import { FaQuoteLeft } from "react-icons/all"
+
+import dp1 from "../../assets/images/dp1.jpeg"
+import dp2 from "../../assets/images/dp2.jpeg"
+import dp3 from "../../assets/images/dp3.jpeg"
+import dp4 from "../../assets/images/dp4.jpeg"
 
 import "swiper/swiper.scss"
 
 import "./testimonies.sass"
+
+SwiperCore.use([Pagination, Navigation])
+
+let stories = [
+  {
+    id: 1,
+    name: "Salum Rajab",
+    dp: dp1,
+    story:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget duis velit, nibh sit nam odio. Ante nunc mi magnis semper condimentum dui.Lorem ipsum",
+  },
+  {
+    id: 2,
+    name: "Veronica Michael",
+    dp: dp2,
+    story:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget duis velit, nibh sit nam odio. Ante nunc mi magnis semper condimentum dui.Lorem ipsum",
+  },
+  {
+    id: 3,
+    name: "Elineka Maleo",
+    dp: dp3,
+    story:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget duis velit, nibh sit nam odio. Ante nunc mi magnis semper condimentum dui.Lorem ipsum",
+  },
+  {
+    id: 4,
+    name: "Justine Mahinyila",
+    dp: dp4,
+    story:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget duis velit, nibh sit nam odio. Ante nunc mi magnis semper condimentum dui.Lorem ipsum",
+  },
+  {
+    id: 5,
+    name: "Jenny McCurthy",
+    dp: dp2,
+    story:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget duis velit, nibh sit nam odio. Ante nunc mi magnis semper condimentum dui.Lorem ipsum",
+  },
+]
+
 const Testimonies = () => {
   return (
     <section className="testimonies">
@@ -13,16 +60,26 @@ const Testimonies = () => {
       </div>
       <div className="stories">
         <div className="showcase">
-          <Swiper spaceBetween={0} grabCursor={true}>
-            <SwiperSlide>
-              <article className="card">Hello</article>
-            </SwiperSlide>
-            <SwiperSlide>
-              <article className="card">Hello</article>
-            </SwiperSlide>
-            <SwiperSlide>
-              <article className="card">Hello</article>
-            </SwiperSlide>
+          <Swiper
+            spaceBetween={0}
+            grabCursor={true}
+            slidesPerView={1}
+            Navigation
+            pagination={{ clickable: true }}
+          >
+            {stories.map(({ id, story, name, dp }) => (
+              <SwiperSlide key={id}>
+                <article className="story">
+                  <div className="home-content">
+                    <p>{story}</p>
+                    <h3>{name}</h3>
+                  </div>
+                  <div className="dp">
+                    <img src={dp} alt={name} />
+                  </div>
+                </article>
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </div>
